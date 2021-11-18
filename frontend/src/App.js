2581b20093798476e94a87d6e5fe95f999cdd63d
Jules Gribble <jules.gribble@toca.io>
2021-11-18 20:13:29 +0000

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from './toca_logo_solid_pinkandwhite.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hackathon
+        </p>
+        <div className="App-Subtitle">
+          This works bro
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
