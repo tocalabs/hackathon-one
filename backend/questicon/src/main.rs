826a14a5d3119533d748318f22f38071fb42692f
james swash <james.swash@tocabot.io>
2021-11-18 20:30:33 +0000

    Web server config and character init

--- backend/questicon/src/main.rs
+++ backend/questicon/src/main.rs
@@ -1,5 +1,19 @@
 #[macro_use] extern crate rocket;
 
+
+
+struct Character {
+    name: String,
+    level: i16,
+    is_dead: bool
+}
+
+impl Character {
+    fn new(name: String) -> Self {
+        Character {name, level: 0, is_dead: false}
+    }
+}
+
 #[get("/")]
 fn hello() -> &'static str {
     "Hi there!"
