8ce91298cf090636153baecad6a29973e4f7bb7e
Jules Gribble <gribblej@me.com>
2021-11-18 21:02:56 +0000

    Home and confetti

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactConfetti from 'react-confetti';
+
+export default function Home() {
+    return <ReactConfetti width={window.width} height={window.height} />
+}
