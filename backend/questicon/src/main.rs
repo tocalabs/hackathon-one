5b5c3ae73219023d35b2c462228f1ad238ea0a7b
james swash <james.swash@tocabot.io>
2021-11-18 20:33:24 +0000

    Creating protagonist

--- backend/questicon/src/main.rs
+++ backend/questicon/src/main.rs
@@ -12,10 +12,16 @@ impl Character {
     pub fn new(name: String) -> Self {
         Character {name, level: 0, is_dead: false}
     }
+
+    fn increment_level(&mut self) {
+        self.level += 1;
+    }
 }
 
 #[get("/")]
 fn hello() -> &'static str {
+    let mut protagonist = Character::new("James".to_string());
+    protagonist.increment_level();
     "Hi there!"
 }
 
