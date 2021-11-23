14c11bd7bc869a50a46b3013a55e57eca77fb04b
James S <james.swash@tocabot.io>
2021-11-23 21:48:48 +0000

    operational overload testing

--- backend/questicon/src/main.rs
+++ backend/questicon/src/main.rs
@@ -1,46 +1,41 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 use std::ops;
 
+#[derive(Debug, PartialEq)]
 struct Xp(i8);
 
 trait Limit {
-    fn max() -> Self {
-        Self(100)
+    fn max() -> i8 {
+        100
     }
-    fn min() -> Self {
-        Self(0)
+    fn min() -> i8 {
+        0
     }
 }
 
-impl Limit for Xp {
-    fn max() -> Self {
-        Self(100)
-    }
-    fn min() -> Self {
-        Self(0)
-    }
-}
+impl Limit for Xp {}
 
 impl ops::Add for Xp {
-    type Output = i8;
+    type Output = Self;
     fn add(self, rhs: Self) -> Self::Output {
         let total = self.0 + rhs.0;
         if total >= Self::max() {
-            return total - Self::max()
+            return Self(total - Self::max());
         }
-        total
+        Self(total)
     }
 }
 
 impl ops::Sub for Xp {
-    type Output = i8;
+    type Output = Self;
     fn sub(self, rhs: Self) -> Self::Output {
         let total = self.0 - rhs.0;
         if total <= Self::min() {
-            return Self::min();
+            return Self(Self::min());
         }
-        total
+        Self(total)
     }
 }
 
@@ -48,12 +43,12 @@ trait Survival {
     fn attack() -> i16;
     fn defend() -> i16;
 }
-
+#[derive(Debug, PartialEq)]
 struct Character {
     name: String,
     level: i16,
     xp: Xp,
-    is_dead: bool
+    is_dead: bool,
 }
 
 impl Default for Character {
@@ -62,14 +57,19 @@ impl Default for Character {
             name: "".to_string(),
             level: 0,
             xp: Xp(0),
-            is_dead: false
+            is_dead: false,
         }
     }
 }
 
 impl Character {
     pub fn new(name: String) -> Self {
-        Character {name, level: 0, is_dead: false, ..Default::default()}
+        Character {
+            name,
+            level: 0,
+            is_dead: false,
+            ..Default::default()
+        }
     }
     fn increment_level(&mut self) {
         self.level += 1;
@@ -91,7 +91,31 @@ mod tests {
     use super::*;
 
     #[test]
-    fn default_player(){
-        assert_eq!(Character::default(), Character {"".to_string(), });
+    fn default_player() {
+        assert_eq!(
+            Character::default(),
+            Character {
+                name: "".to_string(),
+                level: 0,
+                xp: Xp(0),
+                is_dead: false
+            }
+        );
+    }
+
+    #[test]
+    fn xp_ceiling() {
+        let original_xp = Xp(90);
+        let new_xp = Xp(20);
+        let added_xp = original_xp + new_xp;
+        assert_eq!(added_xp, Xp(10));
+    }
+
+    #[test]
+    fn xp_floor() {
+        let low_xp = Xp(20);
+        let negative_xp = Xp(25);
+        let subtracted_xp = low_xp - negative_xp;
+        assert_eq!(subtracted_xp, Xp(0));
     }
 }
