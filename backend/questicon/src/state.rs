a0efa988215c7010f0ad82a32190fd55a8cd0cd3
James S <james.swash@tocabot.io>
2021-12-10 19:07:13 +0000

    Fleshed out some structs

--- backend/questicon/src/state.rs
+++ backend/questicon/src/state.rs
@@ -21,7 +21,61 @@ pub struct Character;
 /// 2. Description
 /// 3. Type (enum of type of envs)
 /// 4. List of events that can happen to the character
-pub struct Page;
+pub struct Page {
+    name: String,
+    description: String,
+    kind: Environment,
+    events: Vec<Event>,
+}
+
+/// Environment descibes what type of biome the character finds themselves in
+///
+/// Enum is size of usize - it stores a number representing which variant is selected
+pub enum Environment {
+    City,
+    Outskirts,
+    Forest,
+    Cavern,
+}
+
+trait Move {
+    fn move_env(&self) -> Self;
+}
+
+impl Move for Environment {
+    fn move_env(&self) -> Self {
+        match &self {
+            Environment::City => {}
+            Environment::Outskirts => {}
+            _ => {
+                println!("I've moved!!")
+            }
+        }
+
+        Self::City
+    }
+}
+
+/// Event dictates the type of event your character is interacting with
+pub enum Event {
+    /// Moving from one page to another
+    Transition { next_room: Environment },
+    /// Something that happens to the character
+    Encounter { kind: EncounterType },
+}
+
+pub enum EncounterType {
+    Combat,
+    Friendly,
+    BinaryDecision(String),
+    UseItem(Item),
+    Puzzle(String),
+}
+
+pub struct Item {
+    damage: usize,
+    health_restoration: usize,
+}
 
 /// Previous state will contain previous page and previous character
 ///
